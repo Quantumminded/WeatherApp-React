65c839e239fe37ced5aeb7d1511235e5df2dd4fb
Luca <lukkino31@gmail.com>
2022-11-14 20:50:25 +0100

    create input bar with style

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
 import React, { useState } from 'react'
-import './App.css'
+
 
 function App() {
 
-  const apiKey = '6cbae67f2f27d83ccede5cf2d7de4f00'
-  const [weatherData, setWeatherData] = useState([{}])
-  const [city, setCity] = ("")
-
-  const getWeather = (event) => {
-    if (event.key === "Enter") {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}`)
-      .then(
-        response => response.json()
-        ).then(
-          data => {
-            setWeatherData(data)
-            setCity("")
-            console.log(setWeatherData(data))
-          }
-        )
-    }
+  const api = {
+    key: "6cbae67f2f27d83ccede5cf2d7de4f00",
+    base: "http://api.openweathermap.org/data/2.5/"
   }
   
-  return (
-    <div className='container'>
-      <input 
-      className='input' 
-      placeholder='Enter City....'
-      onChange={e => setCity(e.target.value)}
-      value={city}
-      onKeyPress={getWeather}
-       />
 
-       {typeof weatherData.main === 'undefined' ? (
-        <div>
-          <p>Welcome to weather App</p>
+  
+  
+  return (
+    <div className='app'>
+      <main>
+        <div className='search-box'>
+          <input
+             type="text"
+             className="search-bar"
+             placeholder='Search...'>
+          </input>
         </div>
-       ) : (
-        <div>
-          <p>{weatherData.name}</p>
-          <p>{Math.round(weatherData.main.temp)}F</p>
-          <p>{weatherData.weather[0].main}</p>
-        </div>
-       )}
+      </main>
     </div>
   )
 }
